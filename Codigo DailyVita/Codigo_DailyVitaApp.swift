de7ec1d98346bc7ad6d2882dd103e5c5cfc02e02
ibrahim <ibrdrahim@gmail.com>
2024-03-16 09:54:07 +0700

    Initial Commit

---
+++ Codigo DailyVita/Codigo_DailyVitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codigo_DailyVitaApp.swift
+//  Codigo DailyVita
+//
+//  Created by Ibrahim Baisa on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Codigo_DailyVitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
